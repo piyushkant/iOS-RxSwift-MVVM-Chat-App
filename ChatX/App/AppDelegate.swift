4b37bf12e258f57454d5c8a1c4eef9e4c089b80b
Piyush Kant <pkreaper@gmail.com>
2021-05-03 17:22:17 +0900

    add groups

---
+++ ChatX/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ChatX
+//
+//  Created by Piyush Kant on 2021/05/03.
+//
+
+import UIKit
+import Firebase
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
